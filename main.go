06bb1f9f155382cfd594bdf3c3ef671e4c5410f2
Ivan <121018313+Feof1l@users.noreply.github.com>
2023-06-19 18:41:03 +0300

    Add files via upload

--- main.go
+++ main.go
@@ -1,32 +1,107 @@
 package main
 
 import (
-	"log"
-	"time"
+	"context"
+	"errors"
+	"fmt"
+	"io"
+	"io/ioutil"
+	"net"
+	"net/http"
 )
 
+const keyServerAddr = "serverAddr" //ключ для значения адреса HTTP-сервера в контексте
+
 func main() {
-	now_date := time.Now()
-	log.Println("Main started")
+	mux1 := http.NewServeMux()
+	mux1.HandleFunc("/", getRoot)
+	mux1.HandleFunc("/hello", getHello)
 
-	log.Println("Current date and time is: ", now_date.Format("2006-01-02 15:04:05"))
+	mux2 := http.NewServeMux()
+	//mux2.HandleFunc("/", getRoot)
+	//mux2.HandleFunc("/hello", getHello)
 
-	/*ch := make(chan struct{})
-	go func(c chan struct{}) {
-		Run()
-		close(c)
-	}(ch)
-	<-ch
-	*/
-	Run()
-	time.Sleep(1 * time.Second)
-	var test1 checker = tcp_prov{}
-	var test2 checker = http_prov{}
-	test1.check("127.0.0.1", "3333")
-	test2.check("127.0.0.1", "3333")
+	ctx := context.Background()
+	serverOne := &http.Server{ // первый сервер
+		Addr:    ":3333",
+		Handler: mux1,
+		BaseContext: func(l net.Listener) context.Context { // функция изменения контекста
+			ctx = context.WithValue(ctx, keyServerAddr, l.Addr().String())
+			return ctx
+		},
+	}
 
-	test1.check("127.0.0.1", "4444")
-	test2.check("127.0.0.1", "4444")
+	serverTwo := &http.Server{ //второй сервер
+		Addr:    ":4444",
+		Handler: mux2,
+		BaseContext: func(l net.Listener) context.Context {
+			ctx = context.WithValue(ctx, keyServerAddr, l.Addr().String())
+			return ctx
+		},
+	}
 
-	log.Println("Main out!")
+	go erorrs_er(serverOne)
+	go erorrs_er(serverTwo)
+
+	/*go func() {
+		err := serverOne.ListenAndServe()
+		if errors.Is(err, http.ErrServerClosed) { //завершение работы сервера
+			fmt.Printf("server one closed\n")
+		} else if err != nil { //иная ошибка
+			fmt.Printf("error listening for server one: %s\n", err)
+		}
+		cancelCtx()
+	}()
+
+	go func() {
+		err := serverTwo.ListenAndServe()
+		if errors.Is(err, http.ErrServerClosed) {
+			fmt.Printf("server two closed\n")
+		} else if err != nil {
+			fmt.Printf("error listening for server two: %s\n", err)
+		}
+		cancelCtx()
+	}()*/
+
+	<-ctx.Done() // читаем из каналом перед возвратом в main
+	// продолжаем рабоут пока не завершится любая из подпрогармм сервера
+	//и не будет вызвано аннулирвоание контекста
+	//как только будет вызов cancelC
+
+}
+func erorrs_er(server *http.Server) {
+	err := server.ListenAndServe()
+	if errors.Is(err, http.ErrServerClosed) {
+		fmt.Printf("server two closed\n")
+	} else if err != nil {
+		fmt.Printf("error listening for server: %s\n", err)
+	}
+}
+
+// функции обработчики
+func getRoot(w http.ResponseWriter, r *http.Request) {
+	ctx := r.Context() // создаем контекст для отслеживания серверов
+
+	first := r.URL.Query().Get("first")
+	second := r.URL.Query().Get("second")
+
+	body, err := ioutil.ReadAll((r.Body))
+	if err != nil {
+		fmt.Printf("i can not read body: %s\n", err)
+	}
+
+	fmt.Printf("%s: got / request.first=%s, second=%s, body:\n%s\n", ctx.Value(keyServerAddr), first, second, body)
+	io.WriteString(w, "This is my website!\n")
+}
+func getHello(w http.ResponseWriter, r *http.Request) {
+	ctx := r.Context()
+
+	fmt.Printf("%s: got /hello request\n", ctx.Value(keyServerAddr))
+	myName := r.PostFormValue("myName")
+	if myName == "" {
+		w.Header().Set("x-missing-field", "myName")
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+	io.WriteString(w, fmt.Sprintf("Hello, %s!\n", myName))
 }
