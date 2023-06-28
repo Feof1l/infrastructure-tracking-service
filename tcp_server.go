ce8b5240a2cb86fc15b2600f2dcb78d8d0d23e15
Ivan <121018313+Feof1l@users.noreply.github.com>
2023-06-28 21:37:32 +0300

    Add files via upload

--- tcp_server.go
+++ tcp_server.go
@@ -1,7 +1,7 @@
 package main
 
 import (
-	"fmt"
+	"log"
 	"net"
 )
 
@@ -9,41 +9,82 @@ import (
 func process(conn net.Conn) {
 	defer conn.Close()
 	for {
+
 		// считываем полученные в запросе данные
 		input := make([]byte, (1024 * 4))
 		n, err := conn.Read(input)
 		if n == 0 || err != nil {
-			fmt.Println("Read error:", err)
+			log.Println("Read error:", err)
 			break
 		}
+
 		// выводим на консоль сервера диагностическую информацию
-		fmt.Println(string(input))
+		log.Println(string(input))
 		// отправляем данные клиенту
 		//time.Sleep(time.Second * 6)
 		conn.Write([]byte(input))
 	}
 }
-
+func shutdown(done chan bool, ln net.Listener) chan bool {
+	done <- true // We can advance past this because we gave it buffer of 1
+	ln.Close()   // Now it the Accept will have an error above
+	return done
+}
 func main() {
-
-	fmt.Println("Start server...")
+	stop_chan := make(chan bool, 1)
+	log.Println("Start server...")
 
 	// Устанавливаем прослушивание порта
 	ln, err1 := net.Listen("tcp", ":1111")
 	if err1 != nil {
-		fmt.Println(err1.Error())
+		log.Fatal(err1.Error())
 	}
+	//log.Println("Start server...")
 
 	// Запускаем цикл обработки соединений
 	for {
 		// Принимаем входящее соединение
+		//stop_chan = shutdown(stop_chan, ln)
 		conn, err2 := ln.Accept()
 		if err2 != nil {
-			fmt.Println(err2.Error())
+			select {
+			case <-stop_chan:
+				log.Println("Start is stoped...")
+			default:
+				log.Fatal("Accept failed:", err2.Error())
+			}
+			return
+		}
+		/*if err2 != nil {
+			log.Println(err2.Error())
 			conn.Close()
 			continue
-		}
+		}*/
 		// запускаем функцию process(conn)   как горутину
 		go process(conn)
 	}
 }
+
+/*
+select {
+		//case <-stopChan:
+		//	log.Println("Stopping server")
+		//	return
+		case <-stop:
+			go func() {
+				defer signal.Stop(stop)
+				sig := <-stop
+				action()
+			}()
+		default:
+			// Принимаем входящее соединение
+			conn, err2 := ln.Accept()
+			if err2 != nil {
+				log.Println(err2.Error())
+				conn.Close()
+				continue
+			}
+			// запускаем функцию process(conn)   как горутину
+			go process(conn)
+		}
+	}*/
