b6ff1dbc2a6968cea94ace6a929f0fd595b8ff1f
Ivan <121018313+Feof1l@users.noreply.github.com>
2023-07-03 20:01:53 +0300

    Add files via upload

--- servers/conn.go
+++ servers/conn.go
@@ -8,19 +8,17 @@ import (
 
 type conn struct {
 	net.Conn
-	Timeout       time.Duration
+
 	IdleTimeout   time.Duration
 	MaxReadBuffer int64
 }
 
 func (c *conn) Write(p []byte) (n int, err error) {
-	c.SetWriteDeadline(time.Now().Add(c.Timeout))
 	n, err = c.Conn.Write(p)
 	return
 }
 
 func (c *conn) Read(b []byte) (n int, err error) {
-	c.SetWriteDeadline(time.Now().Add(c.Timeout))
 	r := io.LimitReader(c.Conn, c.MaxReadBuffer)
 	n, err = r.Read(b)
 	return
